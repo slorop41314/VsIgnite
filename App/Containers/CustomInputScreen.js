3af48da758d0d5b7ff165b808bd1a40faa309f3d
Rahmat Zulfikri <me@rahmatzulfikri.xyz>
2019-10-22 17:26:50 +0700

    add text input action example

--- App/Containers/CustomInputScreen.js
+++ App/Containers/CustomInputScreen.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
-import { ScrollView, Text, KeyboardAvoidingView, View } from 'react-native'
+import { ScrollView, Text, KeyboardAvoidingView, View, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from '../Redux/YourRedux'
 import { CustomInput, Styled } from 'react-native-awesome-component'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import Icons from 'react-native-vector-icons/FontAwesome5'
 
 // Styles
 import styles from './Styles/CustomInputScreenStyle'
+import { Colors } from '../Themes'
 
 class CustomInputScreen extends Component {
   constructor(props) {
@@ -20,11 +22,12 @@ class CustomInputScreen extends Component {
       phoneNumber: '',
       description: '',
       qty: '',
+      showPassword: false,
     }
   }
 
   render() {
-    const { userId, password, confirmPassword, email, phoneNumber, description, qty } = this.state
+    const { userId, password, confirmPassword, email, phoneNumber, description, qty, showPassword } = this.state
 
     return (
       <KeyboardAwareScrollView style={{ flex: 1 }} extraScrollHeight={30}>
@@ -39,6 +42,13 @@ class CustomInputScreen extends Component {
             minLength={5}
             value={userId}
             onChangeText={(text) => this.setState({ userId: text })}
+            renderLeftAction={() => {
+              return (
+                <View style={{ marginRight: 10 }}>
+                  <Icons name={'envelope'} color={Colors.steel} size={20} />
+                </View>
+              )
+            }}
           />
           <CustomInput
             placeholder={'Enter Password'}
@@ -49,6 +59,14 @@ class CustomInputScreen extends Component {
             isRequired={true}
             value={password}
             onChangeText={(text) => this.setState({ password: text })}
+            secureTextEntry={showPassword ? false : true}
+            renderRightAction={() => {
+              return (
+                <TouchableOpacity activeOpacity={0.8} style={{ marginLeft: 10 }} onPress={() => this.setState({ showPassword: !showPassword })}>
+                  <Icons name={showPassword ? 'eye' : 'eye-slash'} color={Colors.steel} size={20} />
+                </TouchableOpacity>
+              )
+            }}
           />
           <CustomInput
             placeholder={'Re Enter Password'}
