ae5395ad7dc52f68d4c007264cef2293fe5eb2bd
Rahmat Zulfikri <me@rahmatzulfikri.xyz>
2020-01-09 12:25:39 +0700

    fix ios configuration

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { ScrollView, Text, Image, View, Button } from 'react-native'
+import { ScrollView, Text, Image, View, Button, Platform } from 'react-native'
 import { Images } from '../Themes'
 import { VideoPlayer, Trimmer } from 'react-native-video-processing';
 import ImagePicker from 'react-native-image-picker';
@@ -22,7 +22,9 @@ export default class LaunchScreen extends Component {
       // Same code as in above section!
       console.log({ response })
       if (response) {
-        this.setState({ video: response })
+        if (!response.didCancel) {
+          this.setState({ video: response })
+        }
       }
     });
   }
@@ -69,22 +71,22 @@ export default class LaunchScreen extends Component {
   }
   render() {
     const { video } = this.state
-    console.log({ video })
+    console.tron.error({ video })
     return (
-      <View style={styles.mainContainer}>
+      <View style={[styles.mainContainer, { marginTop: Platform.OS === 'ios' ? 20 : 0 }]}>
         {video !== undefined && (
           <View style={{ backgroundColor: 'red', flex: 1 }}>
             <VideoPlayer
               ref={ref => {
                 this.videoPlayerRef = ref
-                console.log({ref})
+                console.log({ ref })
               }}
               // startTime={30}  // seconds
               // endTime={120}   // seconds
               play={true}     // default false
               replay={true}   // should player play video again if it's ended
               rotate={true}   // use this prop to rotate video if it captured in landscape mode iOS only
-              source={`file://${video.path}`}
+              source={Platform.OS === 'ios' ? video.uri : `file://${video.path}`}
               playerWidth={300} // iOS only
               playerHeight={500} // iOS only
               style={{ width: 300, height: 500 }}
@@ -93,14 +95,14 @@ export default class LaunchScreen extends Component {
               onChange={({ nativeEvent }) => console.log({ nativeEvent })} // get Current time on every second
             />
             <Trimmer
-              source={`file://${video.path}`}
+              source={Platform.OS === 'ios' ? video.uri : `file://${video.path}`}
               height={100}
               width={300}
-              onTrackerMove={(e) => console.log(e.currentTime)} // iOS only
+              // onTrackerMove={(e) => console.log(e.currentTime)} // iOS only
               // currentTime={this.video.currentTime} // use this prop to set tracker position iOS only
-              themeColor={'white'} // iOS only
-              thumbWidth={30} // iOS only
-              trackerColor={'green'} // iOS only
+              // themeColor={'white'} // iOS only
+              // thumbWidth={30} // iOS only
+              // trackerColor={'green'} // iOS only
               onChange={(e) => console.log(e.startTime, e.endTime)}
             />
           </View>
---
+++ ios/VsIgnite-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  VsIgnite-Bridging-Header.h
+//  VsIgnite
+//
+//  Created by Rahmat Zulfikri on 09/01/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#ifndef VsIgnite_Bridging_Header_h
+#define VsIgnite_Bridging_Header_h
+
+#import "RNVideoProcessing.h"
+
+#endif /* VsIgnite_Bridging_Header_h */
