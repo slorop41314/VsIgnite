1a9bd016cee832ee8bd9216e6f4defb4e11b8382
Rahmat Zulfikri <me@rahmatzulfikri.xyz>
2020-01-19 23:00:36 +0700

    add firebase storage & image picker dep

--- android/app/src/main/java/com/vsignite/dev/MainApplication.java
+++ android/app/src/main/java/com/vsignite/dev/MainApplication.java
@@ -21,6 +21,7 @@ import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
 import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage; 
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
+import io.invertase.firebase.storage.RNFirebaseStoragePackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -46,6 +47,7 @@ public class MainApplication extends Application implements ReactApplication {
         packages.add(new RNFirebaseNotificationsPackage());
         packages.add(new RNFirebaseAuthPackage()); 
         packages.add(new RNFirebaseFirestorePackage());
+        packages.add(new RNFirebaseStoragePackage()); 
         return packages;
       }
 
