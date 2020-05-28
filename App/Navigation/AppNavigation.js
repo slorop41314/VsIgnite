d90d9d642c7ae412362e9ed12a80f221f8f9eae8
Rahmat Zulfikri <me@rahmatzulfikri.xyz>
2020-05-28 20:34:08 +0700

    Revert "init project"
    This reverts commit 0b03062555e29eeff5dd436bb129326e641f246d.

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,22 +1,19 @@
-import {createAppContainer} from 'react-navigation';
-import MainScreen from '../Containers/MainScreen';
-import {createStackNavigator} from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack';
+import LaunchScreen from '../Containers/LaunchScreen'
 
-import styles from './Styles/NavigationStyles';
+import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
-const PrimaryNav = createStackNavigator(
-  {
-    MainScreen: {screen: MainScreen},
-  },
-  {
-    // Default config for all screens
-    headerMode: 'none',
-    initialRouteName: 'MainScreen',
-    navigationOptions: {
-      headerStyle: styles.header,
-    },
-  },
-);
+const PrimaryNav = createStackNavigator({
+  LaunchScreen: { screen: LaunchScreen }
+}, {
+  // Default config for all screens
+  headerMode: 'none',
+  initialRouteName: 'LaunchScreen',
+  navigationOptions: {
+    headerStyle: styles.header
+  }
+})
 
-export default createAppContainer(PrimaryNav);
+export default createAppContainer(PrimaryNav)
